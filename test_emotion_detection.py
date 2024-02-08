761ce0ee74c6639e3424ced7b0dea4a824c66a4d
gemps18 <150620155+gemps18@users.noreply.github.com>
2024-02-08 13:33:00 +0100

    Create test_emotion_detection.py

---
+++ test_emotion_detection.py
@@ -0,0 +1,26 @@
+from EmotionDetection.emotion_detection import emotion_detector
+import unittest
+
+class TestEmotionDetection(unittest.TestCase):
+    def test_emotion_detector(self):
+        input1 = emotion_detector('I am glad this happened') 
+        result1 = 'joy'
+        self.assertEqual(input1['dominante_emotion'], result1)
+
+        input2 = emotion_detector('I am really mad about this')
+        result2 = 'anger'
+        self.assertEqual(input2['dominante_emotion'], result2)
+
+        input3 = emotion_detector('I feel disgusted just hearing about this')
+        result3 = 'disgust'
+        self.assertEqual(input3['dominante_emotion'], result3)
+
+        input4 = emotion_detector('I am so sad about this')
+        result4 = 'sadness'
+        self.assertEqual(input4['dominante_emotion'], result4)
+
+        input5 = emotion_detector('I am really afraid that this will happen')
+        result5 = 'fear'
+        self.assertEqual(input5['dominante_emotion'], result5)
+
+unittest.main()
