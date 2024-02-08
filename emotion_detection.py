d861da22c930db63173182a4e6ca4a8b9c33bd69
gemps18 <150620155+gemps18@users.noreply.github.com>
2024-02-08 13:49:46 +0100

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,3 +7,29 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
     return response.text
+
+    formatted = json.loads(response.text)
+    
+    emotions = formatted['emotionPredictions'][0]['emotion']
+    
+    dominant_emotion = 'default'
+    dominant_value = 0
+
+    for emotion, score in emotions.items():
+        if dominant_value < score:
+            dominant_emotion = emotion
+            dominant_value = score
+
+    response = {}
+
+    print('{') 
+    i=0 
+    for emotion in emotions:
+        print("'",emotion,"': ",emotions[emotion],",", sep='')
+        i = i+1
+        response[emotion] = emotions[emotion]
+    print("'dominant_emotion': '",dominant_emotion,"'",sep='')
+    print("}")
+
+    response['dominant_emotion'] = dominant_emotion
+    return response
