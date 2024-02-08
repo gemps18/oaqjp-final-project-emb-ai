0cdd02cde9827a4a9f7fd8be57e3ef8d29f91b82
gemps18 <150620155+gemps18@users.noreply.github.com>
2024-02-08 14:58:13 +0100

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -3,24 +3,24 @@ import unittest
 
 class TestEmotionDetection(unittest.TestCase):
     def test_emotion_detector(self):
-        input1 = emotion_detector('I am glad this happened') 
+        input1 = emotion_detector('I am glad this happened')
         result1 = 'joy'
-        self.assertEqual(input1['dominante_emotion'], result1)
+        self.assertEqual(input1['dominant_emotion'], result1)
 
         input2 = emotion_detector('I am really mad about this')
         result2 = 'anger'
-        self.assertEqual(input2['dominante_emotion'], result2)
+        self.assertEqual(input2['dominant_emotion'], result2)
 
         input3 = emotion_detector('I feel disgusted just hearing about this')
         result3 = 'disgust'
-        self.assertEqual(input3['dominante_emotion'], result3)
+        self.assertEqual(input3['dominant_emotion'], result3)
 
         input4 = emotion_detector('I am so sad about this')
         result4 = 'sadness'
-        self.assertEqual(input4['dominante_emotion'], result4)
+        self.assertEqual(input4['dominant_emotion'], result4)
 
         input5 = emotion_detector('I am really afraid that this will happen')
         result5 = 'fear'
-        self.assertEqual(input5['dominante_emotion'], result5)
+        self.assertEqual(input5['dominant_emotion'], result5)
 
 unittest.main()
