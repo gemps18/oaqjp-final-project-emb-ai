7048090edf87f81d07df258c59f6908229e297bb
gemps18 <150620155+gemps18@users.noreply.github.com>
2024-02-16 19:50:39 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,7 @@
-"""
-Main server module to serve the EmotionDetection project
-"""
-
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
-
 
 @app.route("/")
 def render_index_page():
@@ -14,7 +9,6 @@ def render_index_page():
     This route renders the index page
     """
     return render_template("index.html")
-
 
 @app.route("/emotionDetector")
 def get_emotion_details():
@@ -28,7 +22,6 @@ def get_emotion_details():
         return "Invalid text! Please try again!."
     # pylint: disable=line-too-long
     return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
