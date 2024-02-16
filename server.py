608ceae55bb59d47511417ffe7d9f9080026191e
gemps18 <150620155+gemps18@users.noreply.github.com>
2024-02-16 19:33:44 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,34 @@
+"""
+Main server module to serve the EmotionDetection project
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask('Emotion Detection')
+app = Flask("Emotion Detection")
+
+
+@app.route("/")
+def render_index_page():
+    """
+    This route renders the index page
+    """
+    return render_template("index.html")
+
 
 @app.route("/emotionDetector")
-def emot_detect():
-    text_to_analyse = request.args.get('textToAnalyze')
-    detector_output = emotion_detector(text_to_analyse)
-    if detector_output['dominant_emotion'] is None:
-        return 'Invalid text! Please try again!.'
+def get_emotion_details():
+    """
+    This route calls emotion_detection module
+    of the EmotionDetection package to get emotion details
+    """
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!."
+    # pylint: disable=line-too-long
+    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}"
 
-    response = "For the given statement, the system response is "
 
-    for emotion, score in detector_output.items():
-        if emotion != 'dominant_emotion':
-            response = response+"'"+emotion+"': "+str(score)+", "
-
-    response = response+". The dominant emotion is "+detector_output['dominant_emotion']
-
-    return response, 200
-
-@app.route('/')
-def render_index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port = 5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
